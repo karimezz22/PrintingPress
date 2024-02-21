6049f547bbb5dfb23d03038d74016caaf8db71b7
karimezz22 <74823379+karimezz22@users.noreply.github.com>
2024-02-21 04:27:35 +0200

    Modify Files

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,30 @@
 // Controller for user authentication
 
 function register(req, res) {
-    // Implement registration 
-  }
-  
-  function login(req, res) {
-    // Implement login 
-  }
-  
-  function logout(req, res) {
-    // Implement logout
-  }
-  
-  module.exports = {
-    register,
-    login,
-    logout
-  };
+  // Implement registration
+  // Validate user inputs
+  // Hash the user's password before storing it in the database.
+  // try to implement email verification to ensure that the user's email address is valid.
+  // insert the user's information into the database after validation
+  // Implement error handling to provide meaningful error messages to users in case of validation failures
+}
+
+function login(req, res) {
+  // Implement login
+  // Validate user inputs
+  // Validate the user's data against the stored data in the database
+  // generate JWT tokens
+  // using secure, HttpOnly cookies to store session tokens.
+  // return a response to the client indicating success.
+
+}
+
+function logout(req, res) {
+  // Implement logout
+}
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,27 @@
 // Controller for product management
 
+function createProduct(req, res) {
+  // Implement to crate product
+  // Validate user inputs
+  // Insert product information to the database
+  // Implement error handling to gracefully handle any errors that may occur during the product creation process.
+  // return a response to the client indicating success
+}
+
 function getAllProducts(req, res) {
-    // Implement to get all products
-  }
-  
-  function getProductById(req, res) {
-    // Implement to get a product by ID
-  }
-  
-  function searchProducts(req, res) {
-    // Implement to search for products
-  }
-  
-  module.exports = {
-    getAllProducts,
-    getProductById,
-    searchProducts
-  };
+  // Implement to get all products
+}
+
+function getProductById(req, res) {
+  // Implement to get a product by ID
+}
+
+function searchProducts(req, res) {
+  // Implement to search for products
+}
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  searchProducts,
+};
