1bf2afe17521874e3803696368058381e209cc84
Ayayoy <aytzhran95@gmail.com>
2024-03-31 15:46:46 +0200

    add order and product controllers

--- utils/validators/orderValidator.js
+++ utils/validators/orderValidator.js
@@ -2,3 +2,47 @@
 const { check } = require("express-validator");
 const validatorMiddleware = require("../../middleware/validatorMiddleware");
 const Order = require("../../models/order");
+const Product = require("../../models/product");
+
+exports.createOrderValidator = [
+  check("user_id")
+    .notEmpty().withMessage("User ID required"),
+
+  check("product.product_id")
+    .notEmpty().withMessage("Product ID required")
+    .custom(async (value, { req }) => {
+      const product = await Product.findById(value);
+      if (!product) {
+        throw new Error("Invalid product ID.");
+      }
+    }),
+
+  check("product.quantity")
+    .notEmpty().withMessage("Quantity required")
+    .isInt({ min: 1 }).withMessage("Quantity must be at least 1"),
+
+  check("product.PDF")
+    .notEmpty().withMessage("PDF link required"),
+
+  check("status")
+    .optional(), // Making status optional for order creation
+
+  check("total_cost")
+    .notEmpty().withMessage("Total cost required")
+    .isFloat({ min: 0 }).withMessage("Total cost must be a positive number"),
+
+  validatorMiddleware,
+];
+
+exports.updateOrderValidator = [
+  check("status")
+    .optional(), // Making status optional for order update
+
+  check("accepted")
+    .optional(), // Making accepted optional for order update
+
+  check("total_cost")
+    .optional(), // Making total_cost optional for order update
+
+  validatorMiddleware,
+];
--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,4 +1,33 @@
-//utils/validators/authValidator.js
 const { check } = require("express-validator");
 const validatorMiddleware = require("../../middleware/validatorMiddleware");
-const Product = require("../../models/product");
+const ProductModel = require("../../models/product");
+
+exports.createProductValidator = [
+  check("name")
+    .notEmpty().withMessage("Product name is required.")
+    .isLength({ min: 2 }).withMessage("Product name must be at least 2 characters long.")
+    .isLength({ max: 50 }).withMessage("Product name cannot exceed 50 characters."),
+
+  check("description")
+    .notEmpty().withMessage("Product description is required."),
+
+  check("image")
+    .notEmpty().withMessage("Product image is required."),
+
+  validatorMiddleware,
+];
+
+exports.updateProductValidator = [
+  check("name")
+    .optional()
+    .isLength({ min: 2 }).withMessage("Product name must be at least 2 characters long.")
+    .isLength({ max: 50 }).withMessage("Product name cannot exceed 50 characters."),
+
+  check("description")
+    .optional(),
+
+  check("image")
+    .optional(),
+
+  validatorMiddleware,
+];
